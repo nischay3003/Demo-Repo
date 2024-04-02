1ad7ca01bec4b9abacdcdf4819513032a7f450a5
Nischay <nischay728@gmail.com>
2024-04-02 18:01:23 +0530

    added new file

---
+++ api.htm/api1.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("done");
+});
+app.listen(3000,()=>{
+    console.log("updated 8");
+});
+mongoose.connect("mongodb+srv://nischay728:<839wI85YiXg6cwxv>@backenddb.876dxl2.mongodb.net/NODE-API?retryWrites=true&w=majority&appName=BackendDB")
+.then(()=>{
+    console.log("Connected to database");
+})
